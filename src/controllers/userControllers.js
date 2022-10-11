870909179a41520e82f53bfd6f7c670c08b43b22
lalan992 <lalankumar992588@gmail.com>
2022-10-11 14:07:01 +0530

    merge

--- src/controllers/userControllers.js
+++ src/controllers/userControllers.js
@@ -1,5 +1,5 @@
-// const UserModel = require("../Models/userModel");
-// const validator = require('../validator/validators');
+const UserModel = require("../Models/userModel");
+const validator = require('../validator/validators');
 
 
 
@@ -7,77 +7,77 @@
 
 
 
-// const createUser = async function (req, res) {
-//     try {
-//         let body = JSON.parse(JSON.stringify(req.body))
+const createUser = async function (req, res) {
+    try {
+        let body = JSON.parse(JSON.stringify(req.body))
 
-//         //Validate body 
-//         if (!validator.isValidBody(body)) {
-//             return res.status(400).send({ status: false, msg: "User body should not be empty" });
-//         }
+        //Validate body 
+        if (!validator.isValidBody(body)) {
+            return res.status(400).send({ status: false, msg: "User body should not be empty" });
+        }
 
-//         let { fname, lname, email, password, phone, address } = body
+        let { fname, lname, email, password, phone, address } = body
 
-//         // Validate fname
-//         if (!validator.isValid(fname)) {
-//             return res.status(400).send({ status: false, message: "fname must be present" })
-//         }
+        // Validate fname
+        if (!validator.isValid(fname)) {
+            return res.status(400).send({ status: false, message: "fname must be present" })
+        }
 
-//         // Validation of fname
-//         if (!validator.isValidName(fname)) {
-//             return res.status(400).send({ status: false, msg: "Invalid fname" })
-//         }
+        // Validation of fname
+        if (!validator.isValidName(fname)) {
+            return res.status(400).send({ status: false, msg: "Invalid fname" })
+        }
 
-//         // Validate lname
-//         if (!validator.isValid(lname)) {
-//             return res.status(400).send({ status: false, message: "lname must be present" })
-//         }
+        // Validate lname
+        if (!validator.isValid(lname)) {
+            return res.status(400).send({ status: false, message: "lname must be present" })
+        }
 
-//         // Validation of lname
-//         if (!validator.isValidName(lname)) {
-//             return res.status(400).send({ status: false, msg: "Invalid lname" })
-//         }
+        // Validation of lname
+        if (!validator.isValidName(lname)) {
+            return res.status(400).send({ status: false, msg: "Invalid lname" })
+        }
 
-//         // Validate email
-//         if (!validator.isValid(email)) {
-//             return res.status(400).send({ status: false, message: "email must be present" })
-//         }
+        // Validate email
+        if (!validator.isValid(email)) {
+            return res.status(400).send({ status: false, message: "email must be present" })
+        }
 
-//         // Validation of email id
-//         if (!validator.isValidEmail(email)) {
-//             return res.status(400).send({ status: false, message: "Invalid email id" })
-//         }
+        // Validation of email id
+        if (!validator.isValidEmail(email)) {
+            return res.status(400).send({ status: false, message: "Invalid email id" })
+        }
 
-//         // Validate password
-//         if (!validator.isValid(password)) {
-//             return res.status(400).send({ status: false, message: "password must be present" })
-//         }
+        // Validate password
+        if (!validator.isValid(password)) {
+            return res.status(400).send({ status: false, message: "password must be present" })
+        }
 
-//         // Validation of password
-//         if (!validator.isValidPassword(password)) {
-//             return res.status(400).send({ status: false, message: "Invalid password" })
-//         }
+        // Validation of password
+        if (!validator.isValidPassword(password)) {
+            return res.status(400).send({ status: false, message: "Invalid password" })
+        }
 
-//         // Validate phone
-//         if (!validator.isValid(phone)) {
-//             return res.status(400).send({ status: false, message: "phone must be present" })
-//         }
+        // Validate phone
+        if (!validator.isValid(phone)) {
+            return res.status(400).send({ status: false, message: "phone must be present" })
+        }
 
-//         // Validation of phone number
-//         if (!validator.isValidNumber(phone)) {
-//             return res.status(400).send({ status: false, msg: "Invalid phone number" })
-//         }
+        // Validation of phone number
+        if (!validator.isValidNumber(phone)) {
+            return res.status(400).send({ status: false, msg: "Invalid phone number" })
+        }
 
-//         // Validate address
-//         if (!address) {
-//             return res.status(400).send({ status: false, message: "Address is required" })
-//         }
-//         address = JSON.parse(address)
-//         if (typeof address != "object") {
+        // Validate address
+        if (!address) {
+            return res.status(400).send({ status: false, message: "Address is required" })
+        }
+        address = JSON.parse(address)
+        if (typeof address != "object") {
 
-//             return res.status(400).send({ status: false, message: "address should be an object" })
-//         }
-//     } catch (err) {
-//         res.status(500).send({ status: false, msg: err.message });
-//     }
-// };
+            return res.status(400).send({ status: false, message: "address should be an object" })
+        }
+    } catch (err) {
+        res.status(500).send({ status: false, msg: err.message });
+    }
+};
